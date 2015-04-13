67d42a9caedf740c1e2941c144b0f61451362224
Matt Ksiazek <matthew.ksiazek@gmail.com>
2015-04-13 10:29:28 -0400

    first commit

---
+++ Shopping_list.cpp
@@ -0,0 +1,193 @@
+/***************************************************************
+*Author: Matthew Ksiazek                                       *
+*Date 04/13/2015                                               *
+*Description: Creates, maintains and outputs .txt file to      *
+* Dropbox for easy retrieval                                   */
+
+// shoppingList.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+void createList();
+void viewList();
+void appendList();
+void removeList();
+void printList();
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Welcome to the shopping list\n";
+	int x = 1;
+	do
+	{
+		cout << "What would you like to do?\n"
+			<< "1.) Create a new list\n2.) View from the list\n3.) Append to the list\n4.) Remove the list\n5.) Print to the list\n6.) Exit\n";
+
+		cin >> x;
+
+		switch (x)
+		{
+		case 1:
+			createList();
+			break;
+		case 2:
+			viewList();
+			break;
+		case 3:
+			appendList();
+			break;
+		case 4:
+			removeList();
+			break;
+		case 5:
+			printList();
+			break;
+		case 6:
+			cout << "Exiting program";
+			return 0;
+		default:
+			cout << "Error. Please enter a number 1-5";
+		}
+	} while (x != 5);
+	return 0;
+}
+
+void createList()
+{
+	string items;
+	ofstream outputFile;
+	outputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+
+	//Now ask user what to add to list
+	int addToList;
+
+	cout << "How many items would you like to add to the list?" << endl;
+	cin >> addToList;
+	
+	//loop once for each item to add
+		for (int count = 1; count <= addToList; count++)
+		{
+			// get the item 
+			cout << "What is item " << count << " ?";
+			cin >> items;
+
+			//write the items to the file
+			outputFile << items << endl;
+		}
+
+
+	//close the file
+	outputFile.close();
+}
+
+void viewList()
+{
+	ifstream inputFile;
+	string item;
+
+	//attempt to open file
+
+	inputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+
+	//if the file opened successfully
+	if (inputFile)
+	{
+		while (inputFile >> item) //boolean test to read from file
+			cout << item << endl << endl;
+
+		inputFile.close();
+	}
+	else // the file could not be opened or found
+	{
+		cout << "Error opening the file.\n";
+	}
+}
+
+void appendList()
+{
+	string items;
+	string addToNewList;
+	ifstream inputFile;
+	inputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+	ofstream outputFile;
+	outputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\temp.txt");
+	//Used to create temporary file holding old list.txt entries
+	if (inputFile)
+	{
+		while (inputFile >> addToNewList) //boolean test to read from file
+			outputFile << addToNewList << endl << endl;
+	}
+	else // the file could not be opened or found
+	{
+		cout << "Error opening the file.\n";
+	}
+
+	//Now ask user what to add to list
+	int addToList;
+
+	cout << "How many items would you like to add to the list?" << endl;
+	cin >> addToList;
+
+	//loop once for each item to add
+	for (int count = 1; count <= addToList; count++)
+	{
+		// get the item 
+		cout << "What is item " << count << " ?" << endl;
+		cin >> items;
+
+		//write the items to the file
+		outputFile << items << endl;
+	}
+
+	// delete the original file
+	remove ("/C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+	// rename old to new
+	rename("C:\\Users\\Matt Ksiazek\\Dropbox\\temp.txt", "C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+
+	//close the file
+	outputFile.close();
+}
+
+void removeList()
+{
+	cout << "Removelist";
+	/*string line, deleteThing;
+	cout << "What would you like to delete?" << endl;
+	cin >> deleteThing;
+
+	ifstream inputFile; 
+	inputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+	ofstream outputFile;
+	outputFile.open("C:\\Users\\Matt Ksiazek\\Dropbox\\temp.txt");
+	
+	
+
+
+	while (getline(in, line))
+	{
+		if (line != deleteThing)
+			outputFile << line; endl;
+	{
+		inputFile.close();
+		outputFile.close();
+		// delete the original file
+		remove("C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+		// rename old to new
+		rename("C:\\Users\\Matt Ksiazek\\Dropbox\\temp.txt", "C:\\Users\\Matt Ksiazek\\Dropbox\\list.txt");
+
+		// all done!
+		return 0;*/
+}
+
+void printList()
+{
+	cout << "This is printing a list";
+}
