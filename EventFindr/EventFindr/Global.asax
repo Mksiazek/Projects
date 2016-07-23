193faac45c4f7eaa8c01740a5fe0bcedf74b5f7f
matt <matt@DESKTOP-NQ0GS28>
2016-07-23 10:10:22 -0400

    Initial commit

---
+++ EventFindr/EventFindr/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventFindr.MvcApplication" Language="C#" %>
---
+++ EventFindr/EventFindr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventFindr.Startup))]
+namespace EventFindr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
